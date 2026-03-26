15a90ea38502e9b95153d99d7c1c74ad05fe0304
mamartam <marta.masiukk@gmail.com>
2026-03-26 12:39:23 +0200

    delete class dark and ligh which was added to to body element, since it is not needed now

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -8,13 +8,9 @@ function applyTheme(savedMode) {
   document.documentElement.setAttribute("data-theme", savedMode);
 
   if (savedMode === "dark") {
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
     moonSunImg.src = SUN_IMG;
     inputCheckBox.checked = true;
   } else {
-    document.body.classList.remove("dark");
-    document.body.classList.add("light");
     moonSunImg.src = MOON_IMG;
     inputCheckBox.checked = false;
   }
